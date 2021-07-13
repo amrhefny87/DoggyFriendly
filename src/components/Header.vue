85046baa9916d9f6d6ebc8369f9dc2ab2d2a3bb0
martindejos <martindejos@gmail.com>
2021-07-13 10:01:57 +0200

    icon navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="nav" class="w-100 p-0 m-0 ">
-        <b-navbar toggleable id="navbar">
-          <router-link to="/">
-            <b-img src="https://i.ibb.co/rQd5Wnz/imageonline-co-whitebackgroundremoved-1.png" alt="doggyF" id="image" />
-          </router-link>
-        <b-navbar-toggle target="navbar-toggle-collapse" type="dark" class="m-4">
-          <template #default="{ expanded }">
-            <b-icon v-if="expanded" icon="chevron-bar-up" ></b-icon>
-            <b-icon v-else icon="chevron-bar-down"></b-icon>
-          </template>
-        </b-navbar-toggle>
-        <b-collapse id="navbar-toggle-collapse"  is-nav>
-          <b-navbar-nav class="ml-auto p-3 d-flex justify-content-start align-items-start">
-            <b-nav-item to="/dogs">Dogs</b-nav-item>
-            <b-nav-item to="/sitters" >Sitters</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+    <b-navbar toggleable id="navbar">
+      <router-link to="/">
+        <b-img
+          src="https://i.ibb.co/rQd5Wnz/imageonline-co-whitebackgroundremoved-1.png"
+          alt="doggyF"
+          id="image"
+        />
+      </router-link>
+      <b-navbar-toggle target="navbar-toggle-collapse" class="m-4">
+        <template #default="{ expanded }">
+          <b-icon
+            v-if="expanded"
+            icon="x"
+          ></b-icon>
+          <b-icon v-else icon="grid3x3-gap-fill"></b-icon>
+        </template>
+      </b-navbar-toggle>
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav
+          class="ml-auto p-3 d-flex justify-content-start align-items-start"
+        >
+          <b-nav-item to="/dogs">Dogs</b-nav-item>
+          <b-nav-item to="/sitters">Sitters</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Header',
-  
-}
-
+  name: "Header",
+};
 </script>
 
 <style>
@@ -49,16 +55,14 @@ export default {
 
 #nav a.router-link-exact-active {
   color: black;
-  }
+}
 
 #image {
-  width: 70px;
+  width: 50px;
   margin: 15px;
 }
 
 #navbar {
-  background: #E07A1B;
+  background: #e07a1b;
 }
-
-
 </style>
