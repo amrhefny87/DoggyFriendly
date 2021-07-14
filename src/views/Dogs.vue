d4306ccc83d788c38332d86d3e4dc7fa291b5624
martindejos <martindejos@gmail.com>
2021-07-14 11:44:20 +0200

    buttons

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dogs">
-    <h1>Dog</h1>
-    <router-link to="/dogs" class="btn text-white btn-primary">Add and Ad</router-link>
+    <h1 class="mt-3">Dog</h1>
+    <router-link to="/dogs" class="btn text-white" id="buttonDogs">Add and Ad</router-link>
     <div v-for="event in events" :key="event.id"  class="d-flex flex-column align-items-center">
           <NewAd :key="event.id"  :result="event" />
     </div>
--- src/views/Sitters.vue
+++ src/views/Sitters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dogs">
-    <h1>Sitters</h1>
-    <router-link to="/dogs" class="btn text-white btn-primary">Add and Ad</router-link>
+    <h1 class="mt-3">Sitters</h1>
+    <router-link to="/dogs" class="btn text-white btn-primary" id="buttonSitters">Add and Ad</router-link>
     <div v-for="event in events" :key="event.id"  class="d-flex flex-column align-items-center">
           <NewAd :key="event.id"  :result="event" />
     </div>
@@ -36,3 +36,14 @@ methods:{
 }
 }
 </script>
+
+<style scoped>
+
+#buttonSitters {
+  background: #e07a1b;
+}
+
+#buttonSitters:hover {
+  background: #e07f24;
+}
+</style>
