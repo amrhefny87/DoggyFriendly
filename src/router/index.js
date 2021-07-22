4fc08d2e772357017773bbb7a6b78467ca053ab7
martindejos <martindejos@gmail.com>
2021-07-22 15:10:15 +0200

    vue auth, if-else in vue routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,102 +1,104 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Dogs from '../views/Dogs.vue'
-import Sitters from '../views/Sitters.vue'
-import FormDogs from '../views/FormDogs.vue'
-import FormSitters from '../views/FormSitters.vue'
-import AdType from '../views/AdType.vue'
-import Profils from '../views/Profils.vue'
-import Show from '../views/Show.vue'
-import EditAdDogs from "../views/EditAdDogs.vue"
-import EditAdSitters from "../views/EditAdSitters.vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Dogs from "../views/Dogs.vue";
+import Sitters from "../views/Sitters.vue";
+import FormDogs from "../views/FormDogs.vue";
+import FormSitters from "../views/FormSitters.vue";
+import AdType from "../views/AdType.vue";
+import Profils from "../views/Profils.vue";
+import Show from "../views/Show.vue";
+import EditAdDogs from "../views/EditAdDogs.vue";
+import EditAdSitters from "../views/EditAdSitters.vue";
 import Login from "../views/Login";
 import Register from "../views/Register";
-import store from "../store"
+import store from "../store";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/sitters',
-    name: 'Sitters',
-    component: Sitters
+    path: "/sitters",
+    name: "Sitters",
+    component: Sitters,
   },
   {
-    path: '/dogs',
-    name: 'Dogs',
-    component: Dogs
+    path: "/dogs",
+    name: "Dogs",
+    component: Dogs,
   },
   {
-    path: '/formdogs',
-    name: 'formdogs',
+    path: "/formdogs",
+    name: "formdogs",
     component: FormDogs,
+  },
+  {
+    path: "/formsitters",
+    name: "formsitters",
+    component: FormSitters,
     beforeEnter: (to, from, next) => {
-      if (store.getters['auth/authenticated']) {
-        return next ({
-          name: "Login"
-        })
+      if (store.getters["auth/authenticated"]) {
+        ({
+          name: "formsitters",
+        });
+      }
+      if (!store.getters["auth/authenticated"]) {
+        ({
+          name: "home",
+        });
       }
       next()
-    }
+    },
   },
   {
-    path: '/formsitters',
-    name: 'formsitters',
-    component: FormSitters,
-    
+    path: "/profils",
+    name: "profils",
+    component: Profils,
   },
   {
-
-    path:'/profils',
-    name:'profils',
-    component: Profils
+    path: "/adtype",
+    name: "adtype",
+    component: AdType,
   },
   {
-    path:'/adtype',
-    name:'adtype',
-    component: AdType
-  },
-  {
-    path:'/show/:id',
-    name:'Show',
+    path: "/show/:id",
+    name: "Show",
     component: Show,
     props: true,
   },
   {
-    path:'/editAdSitters/:id',
-    name:'EditAdSitters',
+    path: "/editAdSitters/:id",
+    name: "EditAdSitters",
     component: EditAdSitters,
     props: true,
   },
   {
-    path:'/editAdDogs/:id',
-    name:'EditAdDogs',
+    path: "/editAdDogs/:id",
+    name: "EditAdDogs",
     component: EditAdDogs,
     props: true,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
-    
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
