bec7ada8e62213a3b4987ad06a3386de9db3daf8
Cris Mouta <crismouta@gmail.com>
2021-07-23 10:21:38 +0200

    add contact button

---
+++ src/components/ButtonContact.vue
@@ -0,0 +1,29 @@
+<template>
+    <button
+        class="btn text-white m-2 rounded-top rounded-bottom"
+        id="buttonContact"
+        @click="goBack()"
+    >
+        Go Back
+    </button>
+</template>
+
+<script>
+export default {
+    name: "buttonContact",
+    methods: {
+        goBack() {
+        window.history.back();
+        },
+    },
+};
+</script>
+
+<style>
+#buttonContact {
+    background: #5c636a;
+}
+#buttonContact:hover {
+    background: #a15106;
+}
+</style>
