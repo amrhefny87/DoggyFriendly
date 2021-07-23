6b51b2ad5a5c9da59e7d7e1356d48ec5e151aa77
amrhefny87 <ahefny1987@gmail.com>
2021-07-23 08:50:26 +0200

    actualizar el front end

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link :to=root class="" id="button1">
+    <router-link :to=root class="btn text-white m-2 rounded-top rounded-bottom" id="button1">
       <img src={{name}}>
     </router-link>
 </template>
