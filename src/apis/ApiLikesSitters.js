1e0086acb2a36b2e65baa776de68c98ecbd026c0
amrhefny87 <ahefny1987@gmail.com>
2021-07-29 22:40:30 +0200

    crear likes por sitters

---
+++ src/apis/ApiLikesSitters.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const url = "http://127.0.0.1:8000/api/likessitters";
+
+export const apilikessitters = {
+  getAll() {
+    return axios.get(url);
+  },
+  like(request) {
+    return axios.post(url, request)
+  },
+  dislike(id) {
+    return axios.delete(url + "/" + id);
+  },
+  getLikes(id){
+    return axios.get(url+"/"+id);
+  }
+}
